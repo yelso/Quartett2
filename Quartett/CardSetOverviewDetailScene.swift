3550647a52093a2e20ad9c2c72ac1b73901aed82
yelso <pdialeh@gmail.com>
2017-12-30 00:17:42 +0100

    added todo to cardSetOverviewDetailScene.swift

--- Quartett/CardSetOverviewDetailScene.swift
+++ Quartett/CardSetOverviewDetailScene.swift
@@ -10,6 +10,14 @@ import Foundation
 import SpriteKit
 
 class CardSetOverviewDetailScene: SKScene {
-    var imgName = "h"
+    var img: SKSpriteNode?
+    var imgName = "bikes2"
+    override func didMove(to view: SKView) {
+        // TODO
+        if let image = self.childNode(withName: "img") as? SKSpriteNode {
+            img = image
+            img?.texture = SKTexture(imageNamed: imgName)
+        }
+    }
 }
 
